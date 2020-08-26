5f5f77eceb525567c07f565247f09c28add14736
Angel Pereira <angel.sp06@gmail.com>
2020-08-25 21:21:30 -0300

    feat(concepts): react crud to ingrate with nodejs api

--- src/App.js
+++ src/App.js
@@ -1,29 +1,72 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { api } from './services/api'
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [values, setValue] = useState({
+    title: '',
+    url: '',
+    techs: ''
+  })
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  },[])
+
+  async function handleAddRepository(e) {
+    e.preventDefault()
+    const responseBody = await api.post('repositories', ({
+      title: values.title,
+      url: values.urls,
+      techs: values.techs
+    }))
+
+    setRepositories([...repositories, responseBody.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+    setRepositories(repositories.filter(repository => repository.id !== id))
+  }
+
+  const handleTitleValue = (event) => {
+    setValue({ ...values, title: event.target.value})
+  }
+
+  const handleUrlValue = (event) => {
+    setValue({ ...values, url: event.target.value})
+  }
+
+  const handleTechsValue = (event) => {
+    setValue({ ...values, techs: event.target.value})
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <form>
+        <input type="text" value={values.title} onChange={handleTitleValue} placeholder='Nome do repositório' name='title'/>
+        <input type="text" value={values.url} onChange={handleUrlValue} placeholder='Url' name='url'/>
+        <input type="text" value={values.techs} onChange={handleTechsValue} placeholder='Tecnologias' name='techs'/>
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </form>
+      <section>
+        <h3>Repositórios</h3>
+        <ul data-testid="repository-list">
+        { 
+          repositories.map((repository) =>  
+            <li key={repository.id}>
+            {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          )
+        }
+        </ul>
+      </section>
     </div>
   );
 }
